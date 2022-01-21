1833145a082f10e90ea1afb96bb5ab9fafc9b755
przkmatt <93327838+przkmatt@users.noreply.github.com>
2022-01-21 17:08:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from dash import Dash
 from layout import layout
 from callbacks import get_callbacks
 
-app = Dash(__name__)
+server = flask.Flask(__name__)
+server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+app = dash.Dash(__name__, server=server)
+
 app.layout = layout
 
 get_callbacks(app)
